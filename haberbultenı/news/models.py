81dd4c2d483e35c3ec3c2d77712e8a33dc135c18
mialtgg <200316021@ogr.cbu.edu.tr>
2024-02-19 12:34:28 +0300

    created Makale model

---
+++ haberbultenı/news/models.py
@@ -0,0 +1,14 @@
+
+from django.db import models
+
+class Makale (models.Model):
+    yazar = models.CharField(max_length=150)
+    baslık = models.CharField(max_length=120)
+    acıklama = models.CharField(max_length=200)
+    metin = models.TextField()
+    sehir = models.CharField(max_length=120)
+    yayımlanma_tarihi = models.DateTimeField(auto_now_add=True)
+    güncellenme_tarihi = models.DateTimeField(auto_now =True)
+
+def __str__(self):
+    return self.baslik
